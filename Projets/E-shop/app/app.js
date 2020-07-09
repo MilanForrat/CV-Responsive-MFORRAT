d58e0e83525e0d361bc05e4c5b3f575c2676f08a
MilanForrat <you@example.commilan.forrat@gmail.com>
2020-07-09 09:48:34 +0200

    correction faute de frappe

--- Projets/E-shop/app/app.js
+++ Projets/E-shop/app/app.js
@@ -4,13 +4,14 @@ window.onscroll = function() {stickyNavBar()};
 // Get the navs
 let navBar1 = document.getElementById("navbar_1");
 let navBar2 = document.getElementById("navbar_2");
+let offsetStart = document.getElementsByClassName('carousel')
 // Get the offset position of the navbar
 let sticky = navBar2.offsetTop;
 
 
 // Add the sticky class
 function stickyNavBar() {
-  if (window.pageYOffset > sticky) {
+  if (window.pageYOffset > offsetStart) {
     navBar1.classList.add("sticky_1");
     navBar2.classList.add("sticky_2");
   } else {
